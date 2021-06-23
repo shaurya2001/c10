63be1a0ae658ebe3d356b5c46bf24b2a610456b2
shaurya2001 <85989640+shaurya2001@users.noreply.github.com>
2021-06-23 15:38:36 +0530

    c9 template background colour changer

--- sketch.js
+++ sketch.js
@@ -1,16 +1,36 @@
-var a = prompt("Enter the first variable: ")
-var b = prompt("Enter the second variable: ")
+var btn_red;
+var btn_green;
+
+var r = 0;
+var g = 0;
+var b = 0;
 
 function setup() {
-  var b2 = createButton("Click here to Swap")
+  createCanvas(3000, 3000);  
 
-  //mousePressed
-  b2.mousePressed(swap)
+  btn_red=createButton("red");
+  btn_red.position(550,50);
+  btn_red.mousePressed(red_bg);
+
+  btn_green=createButton("green");
+  btn_green.position(650,50);
+  btn_green.mousePressed(green_bg);
 }
 
-function swap() {
-  [a, b] = [b, a]
 
-  console.log("The value of a after swap: " + a)
-  console.log("The value of b after swap: " + b)
+function draw(){
+  background(r,g,b);
 }
+
+
+  function red_bg(){
+    r=400;
+    g=0;
+    b=0;
+  }
+
+  function green_bg(){
+    r=0;
+    g=400;
+    b=0;
+  }
